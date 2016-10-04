baf03306069524f0d45de767d356c9d7df183320
SeanCarroll1 <carroll.seann@gmail.com>
2016-10-04 15:06:46 +0100

    Update

--- Redirect/background.js
+++ Redirect/background.js
@@ -16,3 +16,13 @@ chrome.webRequest.onBeforeRequest.addListener(
     },
     ["blocking"]
 );
+
+chrome.browserAction.onClicked.addListener(function() {
+    var w = 440;
+    var h = 220;
+    var left = (screen.width/2)-(w/2);
+    var top = (screen.height/2)-(h/2); 
+
+    chrome.windows.create({'url': 'popup.html', 'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {
+    });
+});
